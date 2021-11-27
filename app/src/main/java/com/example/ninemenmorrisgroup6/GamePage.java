12fffe5cbcf0ba231d49662eca3f5f7c8c447d1e
Boma <bhallida@ualberta.ca>
2021-11-27 10:20:45 +0100

    Added some details to the game_page xml and added a new method to the GamePage class.

--- app/src/main/java/com/example/ninemenmorrisgroup6/GamePage.java
+++ app/src/main/java/com/example/ninemenmorrisgroup6/GamePage.java
@@ -3,6 +3,7 @@ package com.example.ninemenmorrisgroup6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class GamePage extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class GamePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+    }
+
+    /**
+     * Doesn't do anything yet but it will open a pop-up window where the players can change their
+     * pieces
+     * @param view
+     */
+    public void ChangePieces(View view) {
     }
 }
