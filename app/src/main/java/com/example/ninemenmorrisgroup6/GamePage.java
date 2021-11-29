8c55e81cc05fe3ac41b5e762183787c0dc12c618
Boma <bhallida@ualberta.ca>
2021-11-29 15:52:46 +0100

    Added three more methods to the GamePage class.

--- app/src/main/java/com/example/ninemenmorrisgroup6/GamePage.java
+++ app/src/main/java/com/example/ninemenmorrisgroup6/GamePage.java
@@ -2,6 +2,7 @@ package com.example.ninemenmorrisgroup6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,5 +20,21 @@ public class GamePage extends AppCompatActivity {
      * @param view
      */
     public void ChangePieces(View view) {
+    }
+
+    /**
+     * Links the Exit game button to the home page.
+     * @param view
+     */
+    public void ExitGame(View view) {
+        Intent intent = new Intent(this, SetupPage.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Doesn't do anything yet but it will give action to the reset button
+     * @param view
+     */
+    public void ResetGame(View view) {
     }
 }
