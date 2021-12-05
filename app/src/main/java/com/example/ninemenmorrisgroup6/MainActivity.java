5e583fdddaabbbd8ca1921d222efde0d75a99749
Boma <bhallida@ualberta.ca>
2021-12-05 15:34:52 +0100

    Created a RulesPopup.java file and xml, Added a linear layout to the MainActivity.xml that darkens the screen when popup is opened, Created a popup for the rules and linked it to the rules button on the homepage.

--- app/src/main/java/com/example/ninemenmorrisgroup6/MainActivity.java
+++ app/src/main/java/com/example/ninemenmorrisgroup6/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.ninemenmorrisgroup6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer backgroundMusic;
@@ -73,6 +78,39 @@ public class MainActivity extends AppCompatActivity {
         //backgroundMusic.start();
     }
 
+    /**
+     * This Creates the popup for the rules
+     */
+    public void displayPopupWindow() {
+        View layout = getLayoutInflater().inflate(R.layout.activity_rules_popup,null);
+        final PopupWindow popup = new PopupWindow(this);
+        TextView popupText = (TextView) findViewById(R.id.Rules);
+        //setting background dim when showing popup
+        LinearLayout dim_layout = (LinearLayout) findViewById(R.id.dim_layout);
+        popup.setContentView(layout);
+        popup.setOutsideTouchable(true);
+        popup.setFocusable(true);
+        popup.setWidth(1000);
+        popup.setHeight(1500);
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 0);
+        dim_layout.setVisibility(View.VISIBLE);
+        popup.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        Button button = (Button) layout.findViewById(R.id.Return);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View layout) {
+                popup.dismiss();
+                dim_layout.setVisibility(View.GONE);
+            }
+        });
 
+    }
+
+    /**
+     * opens the popup page page for the rules
+     * @param view
+     */
+    public void RulesButton(View view) {
+        displayPopupWindow();
+    }
 }
 
